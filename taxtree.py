bb9023b27d76304590db6483b8b355b69a3ec630
Fredrik Boulund <fredrik.boulund@chalmers.se>
2014-08-13 15:50:58 +0200

    Taxtree implementation using ETE2. get_common_ancestor is awesome!

--- taxtree.py
+++ taxtree.py
@@ -4,84 +4,84 @@
 # Taxonomic trees
 
 from sys import argv, exit
+from string import strip
+import logging
+import os
+import cPickle
+from ete2 import Tree
 
-#if len(argv)<3:
-#    print "usage: script.py TAXDUMP OUTFILE"
-#    exit()
-#else:
-#    taxdump = argv[1]
-#    outfile = argv[2]
+def load_ncbi_tree_from_dump(dumpdir):
+    # Credit goes to jhcepas, code taken from:
+    #   https://github.com/jhcepas/ncbi_taxonomy/blob/master/update_taxadb.py
 
+    namesdump = dumpdir+"names.dmp"
+    nodesdump = dumpdir+"nodes.dmp"
+    if not os.path.isfile(namesdump):
+        raise Exception("Taxdump file {} not found.".format(namesdump))
 
-class Tree:
-    """Tree node (and also entire trees)"""
-    def __init__(self, payload=None, children=[]):
-        self.payload = payload
-        self.children = children
+    parent2child = {}
+    name2node = {}
+    node2taxname = {}
+    synonyms = set()
+    name2rank = {}
+    logging.debug("Loading node names...")
+    for line in open(namesdump):
+        fields =  map(strip, line.split("|"))
+        nodename = fields[0]
+        name_type = fields[3].lower()
+        taxname = fields[1]
+        if name_type == "scientific name":
+            node2taxname[nodename] = taxname
+        elif name_type in set(["synonym", "equivalent name", "genbank equivalent name",
+                               "anamorph", "genbank synonym", "genbank anamorph", "teleomorph"]):
+            synonyms.add( (nodename, taxname) )
+    logging.debug(len(node2taxname), "names loaded.")
+    logging.debug(len(synonyms), "synonyms loaded.")
 
-    def __str__(self):
-        return str(self.payload)
+    logging.debug("Loading nodes...")
+    for line in open(nodesdump):
+        fields =  line.split("|")
+        nodename = fields[0].strip()
+        parentname = fields[1].strip()
+        n = Tree()
+        n.name = nodename
+        n.taxname = node2taxname[nodename]
+        n.rank = fields[2].strip()
+        parent2child[nodename] = parentname
+        name2node[nodename] = n
+    logging.debug(len(name2node), "nodes loaded.")
 
-    def traverse(self, level=0):
-        """Traverse the tree."""
-        pass #do something with tree at level
-        for subtree in self.children:
-            subtree.traverse()
+    logging.debug("Linking nodes...")
+    for node in name2node:
+       if node == "1":
+           t = name2node[node]
+       else:
+           parent = parent2child[node]
+           parent_node = name2node[parent]
+           parent_node.add_child(name2node[node])
+    logging.debug("Tree loaded.")
+    
+    return t, synonyms
 
-    def _print(self, level=0):
-        """Print tree recursively:"""
-        print "{char:<{level}}{payload}".format(char="", level=level, payload=self.payload)
-        for subtree in self.children:
-            subtree._print(level+1)
-
-
-
-def build_test_tree():
-    """Construct a simple test tree, from leaf to root"""
-    leaf1 = Tree("G")
-    parent1 = Tree("CC", [leaf1])
-    parent2 = Tree("TTACCG")
-    parent3 = Tree("A", [parent1, parent2])
-    leaf2 = Tree("CTGATTCCG")
-    parent4 = Tree("C", [leaf2])
-    parent5 = Tree("G")
-    parent6 = Tree("C", [parent4, parent5])
-    tree = Tree("root", [parent3, parent6])
-    return tree
-
-
-def parse_taxdump(taxdump):
-    """Parse NCBI Taxonomy dumpfile (nodes.dmp)"""
-    with open(taxdump) as f:
-        parents = {}
-        for line in f:
-            node_id, parent_id =  line.split("\t|\t")[0:2]
-            parents[node_id] = parent_id
-
-    return parents
 
 if __name__ == "__main__":
-    tt = ['root', 
-            ['A', 
-                ['CC', 
-                    ['G']
-                ], 
-                ['TTACCG']
-            ], 
-            ['C', 
-                ['C', 
-                    ['CTGATTACCG'], 
-                ], 
-                ['G']
-            ], 
-         ]
+
+    if not os.path.exists("taxtree.pkl"):
+        tree, synonyms = load_ncbi_tree_from_dump(dumpdir="/shared/genomes/NCBI/taxonomy/taxdump/")
+        cPickle.dump((tree, synonyms), open("taxtree.pkl", "wb"))
+    else:
+        tree, synonyms = cPickle.load(open("taxtree.pkl"))
+
+    print "Searching for 1280..."
+    aureus = tree.search_nodes(name="1280")
+    agnetis = tree.search_nodes(name="985762")
+    print aureus, agnetis
+    print "Searching for common ancestor to {} {}...".format(aureus[0].name, agnetis[0].name)
+    ancestor = tree.get_common_ancestor([aureus[0].name, agnetis[0].name])
+    print ancestor.name
 
 
-    tree = build_test_tree()
-    tree._print()
 
-
-    parents = sorted(parse_taxdump("/shared/genomes/NCBI/taxonomy/taxdump/nodes.dmp"))
 
 
 
