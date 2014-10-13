511a4906513e51cb03c3f0bd0d6a7b150bf0b3d1
Fredrik Boulund <fredrik.boulund@chalmers.se>
2014-10-13 11:47:46 +0200

    Updated NCBI download scripts for better use interface

--- download_ncbi_genomes.sh
+++ download_ncbi_genomes.sh
@@ -3,9 +3,10 @@
 # Download NCBI microbial FASTA (.fna) and annotations (.gff) current dir and
 # uncompress files
 
-read -p "About to download bacterial genomes from ftp.ncbi.nih.gov\nCtrl-C to abort, Press a key to continue."
-mkdir NCBI_microbial
-cd NCBI_microbial
+date=`date +%Y%m%d`
+read -p "About to download bacterial genomes from ftp.ncbi.nih.gov to bacterial_$date"$'\nCtrl-C to abort, Press a key to continue.'
+mkdir bacterial_$date
+cd bacterial_$date
 wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.fna.tar.gz | tee -a download.log
 wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.gff.tar.gz | tee -a download.log
 gunzip all.fna.tar.gz
--- download_ncbi_taxdump.sh
+++ download_ncbi_taxdump.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # Fredrik Boulund 2014
-# Download NCBI taxonomy dump to current dir and prepare files
+# Download NCBI taxonomy dump to current dir and gunzip files
 
-mkdir taxdump_test
-cd taxdump_test
+date=`date +%Y%m%d`
+read -p "About to download NCBI taxdump from ftp.ncbi.nih.gov to taxdump_$date"$'\nCtrl-C to abort, Press a key to continue.'
+mkdir taxdump_$date
+cd taxdump_$date
 wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_readme.txt | tee -a download_taxdump.log
 wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz | tee -a download_taxdump.log
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid.readme | tee -a download_taxump.log
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid.readme | tee -a download_taxdump.log
 wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz | tee -a download_taxdump.log
 gunzip taxdump.tar.gz | tee -a download_taxdump.log
 gunzip gi_taxid_nucl.dmp | tee -a download_taxdump.log
