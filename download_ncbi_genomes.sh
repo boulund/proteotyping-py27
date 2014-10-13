dbb31757e4d9a47d773e38071f0e90110a07d944
Fredrik Boulund <fredrik.boulund@chalmers.se>
2014-10-13 13:06:15 +0200

    Updated NCBI download scripts with better logging.

--- download_ncbi_genomes.sh
+++ download_ncbi_genomes.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Fredrik Boulund 2014
-# Download NCBI microbial FASTA (.fna) and annotations (.gff) current dir and
-# uncompress files
+# Download NCBI microbial FASTA (.fna) and annotations (.gff) to dir named
+# after today's date and uncompress files
 
 date=`date +%Y%m%d`
-read -p "About to download bacterial genomes from ftp.ncbi.nih.gov to bacterial_$date"$'\nCtrl-C to abort, Press a key to continue.'
-mkdir bacterial_$date
-cd bacterial_$date
-wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.fna.tar.gz | tee -a download.log
-wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.gff.tar.gz | tee -a download.log
-tar -xf all.fna.tar.gz
-tar -xf all.gff.tar.gz
+read -p "About to download bacterial genomes from ftp.ncbi.nih.gov to $date"$'\nCtrl-C to abort, Press a key to continue.'
+mkdir $date
+cd $date
+wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.fna.tar.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
+wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.gff.tar.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
+gunzip all.fna.tar.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
+gunzip all.gff.tar.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
--- download_ncbi_taxdump.sh
+++ download_ncbi_taxdump.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # Fredrik Boulund 2014
-# Download NCBI taxonomy dump to current dir and gunzip files
+# Download NCBI taxonomy dump to dir named after today's date and gunzip files
 
 date=`date +%Y%m%d`
-read -p "About to download NCBI taxdump from ftp.ncbi.nih.gov to taxdump_$date"$'\nCtrl-C to abort, Press a key to continue.'
 mkdir taxdump_$date
 cd taxdump_$date
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_readme.txt | tee -a download_taxdump.log
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz | tee -a download_taxdump.log
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid.readme | tee -a download_taxdump.log
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz | tee -a download_taxdump.log
-tar -xf taxdump.tar.gz | tee -a download_taxdump.log
-gunzip gi_taxid_nucl.dmp.gz | tee -a download_taxdump.log
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_readme.txt > >(tee -a download.log) 2> >(tee -a download.log >&2)
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid.readme > >(tee -a download.log) 2> >(tee -a download.log >&2)
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
+tar -xf taxdump.tar.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
+gunzip gi_taxid_nucl.dmp.gz > >(tee -a download.log) 2> >(tee -a download.log >&2)
